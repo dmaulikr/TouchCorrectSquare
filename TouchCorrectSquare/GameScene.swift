c8f55fe43e80f5190299f454e08cca1e7bc88378
Greg Willis <gregorymwillis@gmail.com>
2016-03-15 21:44:35 -0600

    First Commit, game not working yet, won't recognize correct selection

--- TouchCorrectSquare/GameScene.swift
+++ TouchCorrectSquare/GameScene.swift
@@ -9,37 +9,141 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var leftSquare: SKSpriteNode!
+    var rightSquare: SKSpriteNode!
+    var mainLabel: UILabel!
+    var scoreLabel: SKLabelNode!
+    
+    var customBlue = UIColor(red: 0, green: 0, blue: 0.9, alpha: 1.0)
+    var offWhiteColor = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1.0)
+    var offBlackColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.0)
+    
+    
+    var boxSize: CGFloat {
+        return CGRectGetMaxX(frame) / 2 - 60
+    }
+    
+    var currentScore = 0
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
+        backgroundColor = customBlue
+        spawnSquares()
+        mainLabel = spawnMainLabel()
+        scoreLabel = spawnScoreLabel()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
         
         for touch in touches {
-            let location = touch.locationInNode(self)
+            let touchLocation = touch.locationInNode(self)
+            if let touchedNode = nodeAtPoint(touchLocation) as? SKSpriteNode {
+                if touchedNode.color == offWhiteColor {
+                    addToScore()
+                    spawnSquares()
+                } else {
+                    gameOver()
+                }
+            }
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+}
+
+// MARK: - Spawn Functions
+extension GameScene {
+    
+    func spawnSquares() {
+        var one: UIColor
+        var two: UIColor
+        
+        if randomizeColors() == 0 {
+            one = offWhiteColor
+            two = offBlackColor
+        } else {
+            one = offBlackColor
+            two = offWhiteColor
+        }
+        leftSquare = createSquare(one, size: boxSize, position: CGPoint(x: CGRectGetMidX(frame)-20-boxSize/2, y: CGRectGetMidY(frame)))
+        rightSquare = createSquare(two, size: boxSize, position: CGPoint(x: CGRectGetMidX(frame)+20+boxSize/2, y: CGRectGetMidY(frame)))
+    }
+    
+    func spawnMainLabel() -> UILabel {
+        mainLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view!.frame.width, height: view!.frame.height * 0.4))
+        if let mainLabel = mainLabel {
+            mainLabel.textColor = offWhiteColor
+            mainLabel.font = UIFont(name: "Futura", size: CGRectGetWidth(frame) * 0.14)
+            mainLabel.textAlignment = .Center
+            mainLabel.numberOfLines = 0
+            mainLabel.text = "Choose the WHITE Square"
+            view!.addSubview(mainLabel)
+        }
+        
+        
+        return mainLabel
+    }
+    
+    func spawnScoreLabel() -> SKLabelNode {
+        let score = SKLabelNode(fontNamed: "Futura")
+        score.fontColor = offWhiteColor
+        score.fontSize = CGRectGetWidth(frame) * 0.15
+        score.position = CGPoint(x: CGRectGetMidX(frame), y: CGRectGetMaxY(frame) * 0.2)
+        score.text = "Score: \(currentScore)"
+        
+        addChild(score)
+        
+        
+        return score
+    }
+}
+
+// MARK: - Helper Functions
+extension GameScene {
+    
+    func createSquare(color: UIColor, size: CGFloat, position: CGPoint) -> SKSpriteNode {
+        let square = SKSpriteNode(color: color, size: CGSize(width: size, height: size))
+        square.position = position
+        
+        addChild(square)
+        
+        return square
+    }
+    
+    func addToScore() {
+        currentScore++
+        scoreLabel.text = "Score: \(currentScore)"
+    }
+    
+    func randomizeColors() -> Int {
+        let number = round(random())
+        
+        return Int(number)
+    }
+    
+    func gameOver() {
+        scoreLabel.removeFromParent()
+        mainLabel.text = "Loser!"
+        
+        // TODO: - Launch GameScene
+        let wait = SKAction.waitForDuration(2.0)
+        let transition = SKAction.runBlock {
+            self.mainLabel.removeFromSuperview()
+            if let gameScene = GameScene(fileNamed: "GameScene"), view = self.view {
+                gameScene.scaleMode = .ResizeFill
+                view.presentScene(gameScene, transition: SKTransition.doorwayWithDuration(0.5))
+            }
+        }
+        runAction(SKAction.sequence([wait, transition]))
+
+    }
+    
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / Float(UINT32_MAX))
     }
 }
